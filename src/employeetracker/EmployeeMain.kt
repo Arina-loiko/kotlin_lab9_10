6b3540d74455189766446ad65ced5e46e9c519bf
Arina-loiko <loiko@student.ru>
2026-03-27 20:43:37 +0300

    Demonstrate equals, copy and toString for Task

--- src/employeetracker/EmployeeMain.kt
+++ src/employeetracker/EmployeeMain.kt
@@ -10,4 +10,17 @@ fun main() {
 
     emp.yearsOfExperience = 100
     println("Experience after 100 attempt: ${emp.yearsOfExperience}")
+
+    val task1 = Task("Fix bug", "Fix login bug", Priority.HIGH)
+    val task2 = Task("Fix bug", "Fix login bug", Priority.HIGH)
+    val task3 = Task("Add feature", "Add dark mode", Priority.MEDIUM)
+
+    println("\ntask1 == task2: ${task1 == task2}")
+    println("task1 == task3: ${task1 == task3}")
+    println("task1.hashCode() == task2.hashCode(): ${task1.hashCode() == task2.hashCode()}")
+
+    println("\ntask1: $task1")
+
+    val task1Copy = task1.copy(priority = Priority.LOW)
+    println("task1 copy with LOW priority: $task1Copy")
 }
