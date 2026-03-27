24a6907ce754d3da7fa09f4d5673ba4ff562926e
Arina-loiko <loiko@student.ru>
2026-03-27 20:41:33 +0300

    create Employee class with private fields

---
+++ src/employeetracker/Employee.kt
@@ -0,0 +1,8 @@
+package employeetracker
+
+class Employee(
+    private var fullName: String,
+    private var position: String,
+    private var salary: Int,
+    private var yearsOfExperience: Int
+)
